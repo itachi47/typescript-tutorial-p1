abd4b4ba1cc4898feabf4efea2c4418199a4bfc0
Kuldeep Singh Karhana <kuldeepkarhana21@gmail.com>
2022-09-04 21:49:28 +0530

    Feat: added todo edit, delete and complete feature.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import InputField from "./components/InputField";
+import { Todo } from "./components/model";
+import TodoList from "./components/TodoList";
 
 const App: React.FC = () => {
+  const [todo, setTodo] = React.useState<string>("");
+  const [todos, setTodos] = React.useState<Todo[]>([]);
+  const handleAdd = (event: React.FormEvent) => {
+    event.preventDefault();
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+      setTodo("");
+    }
+  };
   return (
     <div className="App">
-        <span className="heading">Taskify</span>
+      <span className="heading">Taskify</span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList todos={todos} setTodos={setTodos}/>
     </div>
   );
-}
+};
 
 export default App;
